66ba9be907b5856f461d73b06e069e35a9bc9e62
DianoDev <adrianomellocolombo@gmail.com>
2026-01-23 12:35:06 -0400

    configura aplicacao principal
    - Configura providers e interceptors
    - Define rotas principais com lazy loading
    - Atualiza componente raiz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pets',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pets',
+    loadChildren: () => import('./features/pets/pets.routes').then(m => m.PETS_ROUTES)
+  }
+];
